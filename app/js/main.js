a46d1f1d292e01e7828e1104643edce198cc7576
fisherm4n <4143322@inbox.ru>
2021-02-28 20:32:59 +0300

    some adaptive fixes in menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,18 @@
 var swiper1 = new Swiper('.swiper-container-1', {
 
   breakpoints: {
-  
-    767: {
-      slidesPerView: 5,
-    spaceBetween: 10,
-     
-    },
-    1024: {
-      slidesPerView: 6,
-      allowTouchMove: true,
-    },
+    spaceBetween: 30,
+
     1260: {
       slidesPerView: 11,
-    }
+    },
+    
+    767: {
+      slidesPerView: 6,
+    },
+    100: {
+      slidesPerView: 1,
+    },
   }
 });
 var swiper = new Swiper('.swiper-container', {
@@ -25,7 +24,9 @@ var swiper = new Swiper('.swiper-container', {
 });
 
 $(function () {
- 
+  $('.menu__btn').on('click', function () {
+    $('.menu').toggleClass('menu--active');
+  });
   $('.header__categories-title').on('click', function (){
    $('.header__categories-list').slideToggle();
   });
