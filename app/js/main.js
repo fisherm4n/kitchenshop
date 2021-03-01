2121302423dc97cf60a655ec09bfcf6b0aae91c1
fisherm4n <4143322@inbox.ru>
2021-03-01 05:35:25 +0300

    almost finished adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,26 @@
 var swiper1 = new Swiper('.swiper-container-1', {
-
+  setWrapperSize:true,
   breakpoints: {
-    spaceBetween: 30,
-
-    1260: {
-      slidesPerView: 11,
-    },
-    
+    spaceBetween: 10,
     767: {
       slidesPerView: 6,
     },
-    100: {
-      slidesPerView: 1,
+    1260: {
+      slidesPerView: 11,
     },
   }
+  
 });
 var swiper = new Swiper('.swiper-container', {
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
+      navigation: {
+        clickable: true,
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
 
 });
 
@@ -28,10 +29,14 @@ $(function () {
     $('.menu').toggleClass('menu--active');
   });
   $('.header__categories-title').on('click', function (){
-   $('.header__categories-list').slideToggle();
+    $('.header__categories-title').toggleClass("header__categories-title--active");
+    $('.header__categories-list--mobile').slideToggle();
   });
   $('.navigation__title').on('click', function (){
     $(this).next().slideToggle();
   });
-
+  // if ($(".wrapper").width < 800) {
+  //   $('.swiper-wrapper').addClass("disabled");
+  //   $('.swiper-pagination').addClass("disabled");
+  // };
 });
